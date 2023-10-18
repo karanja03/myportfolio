f7aadfc532a2bdbb6e20d17d7bdd3506579e0de0
karanja03 <wambuik03@gmail.com>
2023-10-18 23:38:24 +0300

    Added animation on my homepage

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,10 @@ import React from "react";
 function About(){
     return(
         <div id="about">
-            <h2>This is the about page</h2>
+            <div className="aboutarea">
+                
+            <h2>I'm a versatile full-stack developer with a strong grasp of JavaScript, CSS, HTML, React, Python, Flask, Bootstrap, Tailwind CSS, and database technologies like SQL, PostgreSQL, and SQLite. I'm deeply passionate about front-end development and design, committed to creating elegant, responsive applications. I thrive on collaboration, bringing unique ideas to life, and ensuring exceptional user experiences. I'm here to turn your digital vision into reality.</h2>
+            </div>
         </div>
     )
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,74 @@
-import React from "react";
+// import React from "react";
+
+// function HomePage() {
+//   return(
+//     <div >
+//         <div className="intropart">
+//         <h1 className="myh1">HELLO,</h1>
+//         <h2 className="myh2">I AM WAMBUI KARANJA</h2>
+//         <h3 className="myh3">WELCOME TO MY WORLD</h3>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default HomePage;
+
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function HomePage() {
-  return(
-    <div >
-        <div className="intropart">
-        <h1 className="myh1">HELLO,</h1>
-        <h2 className="myh2">I AM WAMBUI KARANJA</h2>
-        <h3 className="myh3">WELCOME TO MY WORLD</h3>
-        </div>
+  useEffect(() => {
+    gsap.to('.text0', {
+      x: '20%',
+      rotate: 10,
+      duration: 0.85,
+      scrollTrigger: {
+        trigger: '.text0',
+        scrub: true,
+        start: 'top 20%'
+      }
+    });
+
+    gsap.to('.text2', {
+      x: '20%',
+      rotate: 10,
+      duration: 0.85,
+      scrollTrigger: {
+        trigger: '.text2',
+        scrub: true,
+        start: 'top 40%'
+      }
+    });
+
+    gsap.to('.text1', {
+      x: '-20%',
+      rotate: -10,
+      duration: 0.85,
+      scrollTrigger: {
+        trigger: '.text1',
+        scrub: true,
+        start: 'top 30%'
+      }
+      });
+
+    // Add more animations as needed
+
+  }, []); // Ensure this effect runs only once
+
+  return (
+    <div>
+      <div className="intropart">
+
+        < div id ="text" className="text0">HELLO,</div>
+        <div id ="text" className="text1">I AM WAMBUI KARANJA</div>
+        <div id="text"  className="text2">WELCOME TO MY WORLD</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomePage;
